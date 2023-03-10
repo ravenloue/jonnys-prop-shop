260458fc15d714df33fd7311851c67899c6a2a32
ravenloue <42080046+ravenloue@users.noreply.github.com>
2023-03-10 15:06:58 -0600

    About page changes
    Created better organization for images.
    Removed some images from the create-react-app default.
    Remembered to change the page title this time.
    Added the start information for the about page.

---
+++ src/Components/about/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../wrapper/header";
+import Container from "./container";
+
+function About(){
+    return (
+        <div>
+            <Header />
+            <Container />
+        </div>
+    )
+}
+
+export default About;
--- src/Components/wrapper/header.js
+++ src/Components/wrapper/header.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 //
-import title from './../../logo898.png';
-import etsy from './../../etsy.png';
-import insta from './../../insta.png';
-import nav from './../../nav.png';
-import twitter from './../../twitter.png';
+import title from './../../images/logo898.png';
+import etsy from './../../images/etsy.png';
+import insta from './../../images/insta.png';
+import nav from './../../images/nav.png';
+import twitter from './../../images/twitter.png';
 import Navbar from "./nav";
 
 function Header (){
@@ -13,14 +13,17 @@ function Header (){
 
     return(
         <header>
-            <img src={ title } id="title" alt="JonnysPropShop" />
+
+            <a href="/"><img src={ title } id="title" alt="JonnysPropShop" /></a>
             <button className="ico" onClick={() => setOpen(!open)}>
                 <img src={nav} id="nav" alt="nav menu"/></button>
-            <a href="https://www.instagram.com/jonnynumbercinco/" target="_blank"><img src={insta} className="ico" alt="instagram"/></a>
-            <a href="https://www.etsy.com/shop/JonnysToyShop" target="_blank"><img src={etsy} className="ico" alt="etsy shop"/></a>
-            <a href="https://twitter.com/Numberfive18c" target="_blank"><img src={twitter} className="ico" alt="twitter"/></a>
+            <a href="https://www.instagram.com/jonnynumbercinco/" target="_blank" rel="noreferrer"><img src={insta} className="ico" alt="instagram"/></a>
+            <a href="https://www.etsy.com/shop/JonnysToyShop" target="_blank" rel="noreferrer"><img src={etsy} className="ico" alt="etsy shop"/></a>
+            <a href="https://twitter.com/Numberfive18c" target="_blank" rel="noreferrer"><img src={twitter} className="ico" alt="twitter"/></a>
 
-            { open && <Navbar />}
+            
+                { open && <Navbar />}
+
         </header>
     )
 }
